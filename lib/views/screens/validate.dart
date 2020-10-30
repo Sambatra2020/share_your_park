07bffc30111623059f93ce66b2d5e73e051b5ff8
Sambatra2020 <natarabem@gmail.com>
2020-10-30 10:29:55 +0100

    update User To firebase

--- lib/views/screens/validate.dart
+++ lib/views/screens/validate.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
 import 'package:share_your_park/models/user.dart' as userModel;
